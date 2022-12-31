c8898b10038cb78899e150842cb652c016695951
Duleendra Shashimal <duleendra@bambu.co>
2022-12-31 11:07:38 +0800

    Fixed lambda issue

--- lambda-handler/order-invoice/index.mjs
+++ lambda-handler/order-invoice/index.mjs
@@ -1,5 +1,30 @@
-export const handler = async(event) => {
+export const handler = async (event) => {
     console.log("Original event");
     console.log(event);
-    return event;
+
+    const item = event[0].item;
+    const qty = event[0].qty;
+    const price = event[0].price;
+    let discount = 0;
+
+    if (qty >= 100) {
+        discount = "50%";
+    } else if (qty >= 50 && qty < 100) {
+        discount = "30%";
+    } else {
+        discount = "10%";
+    }
+
+    const response = {
+        date: new Date(),
+        item,
+        qty,
+        price,
+        discount
+    };
+
+    console.log("Enriched event");
+    console.log(response);
+
+    return response;
 };

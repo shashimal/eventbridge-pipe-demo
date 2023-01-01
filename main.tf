293c16e7085765a1d4474d1121f7ad822b55dace
Duleendra Shashimal <duleendra@bambu.co>
2023-01-01 19:12:12 +0800

    Removed commented lines

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 locals {
-  filter_pattern            = " {\"body\" :  { \"orderQty\": [{ \"numeric\": [\">\", 10] }] } }"
   enrichment_input_template = "{\"item\": \"<$.body.itemName>\",\"qty\": \"<$.body.orderQty>\", \"price\" : \"<$.body.ItemPrice>\"}"
   filters = [
     {
@@ -7,67 +6,6 @@ locals {
     }
   ]
 }
-
-#resource "aws_cloudformation_stack" "customer_order_pipe_stack" {
-#  name       = "customer-order-pipe-stack"
-#
-#  parameters = {
-#    RoleArn       = aws_iam_role.eventbridge_pipe_sqs_role.arn
-#    SourceArn     = aws_sqs_queue.customer_order_sqs.arn
-#    EnrichmentArn = module.order_process_lambda.lambda_function_arn
-#    TargetArn     = module.order_invoice_lambda.lambda_function_arn
-#    FilterPattern = local.filter_pattern
-#    InputTemplate = local.enrichment_input_template
-#  }
-#
-#  template_body = jsonencode({
-#    "Parameters" : {
-#      "SourceArn" : {
-#        "Type" : "String",
-#      },
-#      "EnrichmentArn" : {
-#        "Type" : "String",
-#      },
-#      "TargetArn" : {
-#        "Type" : "String",
-#      },
-#      "RoleArn" : {
-#        "Type" : "String"
-#      },
-#      "FilterPattern" : {
-#        "Type" : "String"
-#      },
-#      "InputTemplate" : {
-#        "Type" : "String"
-#      }
-#    },
-#
-#    "Resources" : {
-#      "CopyPipe" : {
-#        "Type" : "AWS::Pipes::Pipe",
-#        "Properties" : {
-#          "Name" : "customer-order",
-#          "RoleArn" : { "Ref" : "RoleArn" },
-#          "Source" : { "Ref" : "SourceArn" },
-#          "SourceParameters" : {
-#            "FilterCriteria" : {
-#              "Filters" : [
-#                {
-#                  "Pattern" : { "Ref" : "FilterPattern" },
-#                }
-#              ]
-#            }
-#          },
-#          "Enrichment" : { "Ref" : "EnrichmentArn" },
-#          "EnrichmentParameters" : {
-#            "InputTemplate" : { "Ref" : "InputTemplate" },
-#          }
-#          "Target" : { "Ref" : "TargetArn" }
-#        }
-#      }
-#    }
-#  })
-#}
 
 module "pipe_sqs" {
   source = "./modules/pipe"

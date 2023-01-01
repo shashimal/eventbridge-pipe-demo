7743aa0327878b5df37d8b57128f481a6155a120
Duleendra <shashimald@gmail.com>
2023-01-01 19:12:58 +0800

    Add Pipe module (#1)
    * Add Pipe module
    
    * Removed commented lines
    
    Co-authored-by: Duleendra Shashimal <duleendra@bambu.co>

---
+++ modules/pipe/main.tf
@@ -0,0 +1,52 @@
+resource "aws_cloudformation_stack" "this" {
+  name = "${var.pipe_name}-stack"
+
+  parameters = {
+    RoleArn       = var.pipe_role_arn
+    SourceArn     = var.pipe_source_arn
+    TargetArn     = var.pipe_target_arn
+    EnrichmentArn = var.pipe_enrichment_arn
+    InputTemplate = var.input_transform_template
+  }
+
+  template_body = jsonencode({
+    "Parameters" : {
+      "SourceArn" : {
+        "Type" : "String",
+      },
+      "TargetArn" : {
+        "Type" : "String",
+      },
+      "RoleArn" : {
+        "Type" : "String"
+      },
+      "InputTemplate" : {
+        "Type" : "String"
+      },
+      "EnrichmentArn" : {
+        "Type" : "String",
+      }
+    },
+
+    "Resources" : {
+      "CopyPipe" : {
+        "Type" : "AWS::Pipes::Pipe",
+        "Properties" : {
+          "Name" : var.pipe_name,
+          "RoleArn" : { "Ref" : "RoleArn" },
+          "Source" : { "Ref" : "SourceArn" },
+          "SourceParameters" : {
+            "FilterCriteria" : {
+              "Filters" : var.source_filters,
+            }
+          },
+          "Enrichment" : { "Ref" : "EnrichmentArn" },
+          "EnrichmentParameters" : {
+            "InputTemplate" : { "Ref" : "InputTemplate" },
+          }
+          "Target" : { "Ref" : "TargetArn" }
+        }
+      }
+    }
+  })
+}

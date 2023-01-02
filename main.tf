5499fb26f215c9921a48d931c9f9e80ba1708aad
Duleendra Shashimal <duleendra@bambu.co>
2023-01-02 21:31:16 +0800

    Add enrichment for streaming event

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ locals {
       Pattern: "{\"body\" :  { \"orderQty\": [{ \"numeric\": [\">\", 10] }] } }"
     }
   ]
+
+  #<$.dynamodb\": { \"NewImage\": { \"custom_fields\": { \"S\"} }>
+  dynamodb_enrichment_input_template = "{ \"custom_fields\":  \"<$.dynamodb.NewImage.custom_fields.S.global>\" , \"custom_fields_old\":  \"<$.dynamodb.OldImage.custom_fields.S.global>\"}"
 }
 
 module "pipe_sqs" {
@@ -25,7 +28,7 @@ module "pipe_dynamodb" {
   pipe_source_arn =aws_dynamodb_table.order_info.stream_arn
   pipe_enrichment_arn = module.order_process_lambda.lambda_function_arn
   pipe_target_arn = module.order_invoice_lambda.lambda_function_arn
-  #input_transform_template = local.enrichment_input_template
+  input_transform_template = local.dynamodb_enrichment_input_template
   #source_filters = local.filters
 }
 

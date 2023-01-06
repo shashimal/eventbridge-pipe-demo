8110ce6e3648039b7770315ef9b8f8fc86091c53
Duleendra <duleenw@sph.com.sg>
2023-01-06 14:51:27 +0800

    Removed unwanted code

--- main.tf
+++ main.tf
@@ -1,25 +1,7 @@
 locals {
-  sqs_enrichment_input_template = "{\"item\": \"<$.body.itemName>\",\"qty\": \"<$.body.orderQty>\", \"price\" : \"<$.body.ItemPrice>\"}"
-  filters                       = [
-    {
-      Pattern : "{\"body\" :  { \"orderQty\": [{ \"numeric\": [\">\", 10] }] } }"
-    }
-  ]
-
   aft_modify_event_filters = [{ pattern = "{ \"eventName\": [\"MODIFY\"] }" }]
   aft_enrichment_input_template = "{ \"eventName\": <$.eventName>,  \"newCustomFields\":  <$.dynamodb.NewImage.custom_fields.S> , \"oldCustomFields\":  <$.dynamodb.OldImage.custom_fields.S>}"
 }
-
-#module "pipe_sqs" {
-#  source = "./modules/pipe-sqs"
-#  pipe_name = "order-sqs-pipe"
-#  pipe_role_arn = aws_iam_role.eventbridge_pipe_sqs_role.arn
-#  pipe_source_arn = aws_sqs_queue.customer_order_sqs.arn
-#  pipe_enrichment_arn = module.order_process_lambda.lambda_function_arn
-#  pipe_target_arn = module.order_invoice_lambda.lambda_function_arn
-#  input_transform_template = local.sqs_enrichment_input_template
-#  source_filters = local.filters
-#}
 
 module "pipe_aft" {
   source                   = "./modules/pipe-dynamodb"

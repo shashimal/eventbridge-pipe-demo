6f87cf069122ae022290bedc8d7b9f556f5f2396
Duleendra Shashimal <duleendra@bambu.co>
2023-01-07 23:52:50 +0800

    Added a local variable for app name

--- dynamodb-pipe.tf
+++ dynamodb-pipe.tf
@@ -1,12 +1,13 @@
 locals {
   aft_modify_event_filters = [{ pattern = "{ \"eventName\": [\"MODIFY\"] }" }]
   aft_enrichment_input_template = "{ \"eventName\": <$.eventName>,  \"newCustomFields\":  <$.dynamodb.NewImage.custom_fields.S> , \"oldCustomFields\":  <$.dynamodb.OldImage.custom_fields.S>}"
+  app_name = "dynamodb-pipe"
 }
 
 module "dynamodb_pipe" {
   source                   = "./modules/pipe-dynamodb"
 
-  pipe_name                = "dynamodb-pipe"
+  pipe_name                = local.app_name
   pipe_role_arn            = aws_iam_role.pipe_dynamodb_role.arn
   pipe_source_arn          = aws_dynamodb_table.aft-request.stream_arn
   source_filters           = local.aft_modify_event_filters
